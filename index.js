0ca387d0e7d9faf994b5e0e4f3e07dbd80160a3c
Morris Ravis <morrisravis@berkeley.edu>
2018-07-23 17:27:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ function shout(string) { return string.toUpperCase()
 function whisper(string) { return string.toLowerCase()
 }
 
-function logShout(string) ( console.log(string.toUpperCase())
+function logShout(string) { console.log(string.toUpperCase())
+}
+
+function logWhisper(string) { console.log(string.toLowerCase())
+}
