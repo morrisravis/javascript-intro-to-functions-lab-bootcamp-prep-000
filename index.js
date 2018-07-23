9bfad5e05a5ba09c4e3cd2353d838912a6009b5b
Morris Ravis <morrisravis@berkeley.edu>
2018-07-23 17:54:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) { console.log(string.toLowerCase())
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {return "I can't hear you!";
 
-} else if 
+} else if (string.toUpperCase() === string) {return "Yes Indeed"
 
 
 
