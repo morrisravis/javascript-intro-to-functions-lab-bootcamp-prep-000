f83eedbfabd60edaf030975b28ceabf02f4c69ee
Morris Ravis <morrisravis@berkeley.edu>
2018-07-23 17:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ if (string.toLowerCase() === string) {return "I can't hear you!";
 }
 
 
+function sayHiToGrandma(string) {
+if (string.toLowerCase() === string) {
+return "I can\'t hear you!";
+} else if (string.toUpperCase() === string) {
+return "YES INDEED!";
+} else if (string === "I love you, Grandma.") {return "I love you, too.";
+}
+};
+
 
 
 
