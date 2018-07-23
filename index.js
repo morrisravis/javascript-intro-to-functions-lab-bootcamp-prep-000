e1e7d371d7eb17c04a79656f8f12f91619791ce8
Morris Ravis <morrisravis@berkeley.edu>
2018-07-23 17:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (string.toLowerCase() === string) {return "I can't hear you!";
 
 } else if (string.toUpperCase() === string) {return "YES INDEED!";
 
-} else if (string)
+} else if (string = "I love you, Grandma")
 
 
 
